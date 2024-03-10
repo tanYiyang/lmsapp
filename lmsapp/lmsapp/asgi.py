96363cd9f235a2558ae91f063bc08f62ce890b0c
tanYiyang <tanyiyang2032@gmail.com>
2024-03-11 02:45:58 +0800

    Update asgi.py

--- lmsapp/lmsapp/asgi.py
+++ lmsapp/lmsapp/asgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lmsapp.settings")
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
@@ -16,7 +16,7 @@ from channels.auth import AuthMiddlewareStack
 import courses.routing
 import chat.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lmsapp.settings")
+
 websocket_urlpatterns = courses.routing.websocket_urlpatterns + chat.routing.websocket_urlpatterns
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
