f2ef94fffb76290a1d8c2a163e046cba60b9ed72
Yi Yang <tanyiyang2032@gmail.com>
2024-03-11 00:29:27 +0800

    before switching to postgres

--- lmsapp/lmsapp/settings.py
+++ lmsapp/lmsapp/settings.py
@@ -147,7 +147,7 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels.layers.InMemoryChannelLayer',
