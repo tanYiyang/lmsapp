7bf152a7747ea09a3ecc8af60703b936f943e32b
tanYiyang <tanyiyang2032@gmail.com>
2024-03-11 02:40:25 +0800

    Update urls.py static fix

--- lmsapp/lmsapp/urls.py
+++ lmsapp/lmsapp/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('', include('chat.urls')),
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)”
